f7c06838477a9d30c9c1546afaf2f76e26c7dee3
John Olukunle Ajayi <96730080+johnkunleajayi@users.noreply.github.com>
2024-09-10 23:33:44 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,19 @@ import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
 function App() {
-	return (
-		<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/create' element={<CreatePage />} />
-			</Routes>
-		</Box>
-	);
+  // Define colors based on color mode
+  const bgColor = useColorModeValue("white", "gray.800");
+  const textColor = useColorModeValue("red.600", "red.300"); // Red for text color
+
+  return (
+    <Box minH="100vh" bg={bgColor} color={textColor}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
